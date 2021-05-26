4a6b056aee50659ecfb68bd6560dc2d3024e2606
dunghoang97 <dunghoang.5397@gmail.com>
2021-05-26 17:09:44 +0700

    ok

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - vue-nuxtjs',
+    titleTemplate: '%s - trello-nuxt',
     title: 'trello-nuxt',
     htmlAttrs: {
       lang: 'en',
